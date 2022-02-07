6a37943bf0a4317d32f8dd89d0814d709a9ba027
Tohin <momentohin02@gmail.com>
2022-02-07 23:36:42 +0600

    update

--- Home_App/views.py
+++ Home_App/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from Seller_App.models import Products
+from Login_App.models import SellerProfile
 # Create your views here.
 from django.views import View
 from django.shortcuts import render
@@ -11,4 +12,9 @@ class Index(View):
 
 class Home(View):
     def get(self, request):
-        return render(request, 'home.html',)
+        user = request.user
+        product_post = Products.objects.filter(public=True)
+        seller_list = SellerProfile.objects.all()
+
+        return render(request, 'home.html',
+                      context={'title': 'Admin', 'product_post': product_post, 'seller_list': seller_list})
